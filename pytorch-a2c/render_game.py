3dde464a1c26204ad994c7be99f794f552815118
Angela <angela.denninger@googlemail.com>
2018-03-26 16:22:58 +0100

    render
    Former-commit-id: 55868742b04a9998cb42e7b5aae83200fc7c1e4a

--- pytorch-a2c/render_game.py
+++ pytorch-a2c/render_game.py
@@ -7,12 +7,15 @@ import numpy as np
 from torch.autograd import Variable
 import torch.nn.functional as F
 
+import collections
+
 class RenderAtari():
     def __init__(self, env_id, actor_critic):
         self.actor_critic = actor_critic
         self.env = gym.make(env_id)
         self.env = wrap_deepmind(self.env)
         self.env = WrapPyTorch(self.env)
+        self.states = collections.deque(maxlen=4)
         self.reset()
 
     def render(self):
@@ -22,28 +25,30 @@ class RenderAtari():
         self.actor_critic = actor_critic
 
     def step(self):
-        value, logits = self.actor_critic(Variable(self.state, volatile=True).cuda())
+        states_torch = self.states_to_torch(self.states)
+        value, logits = self.actor_critic(Variable(states_torch, volatile=True).cuda())
         probs = F.softmax(logits)
         log_probs = F.log_softmax(logits).data
         action = probs.multinomial().data
         cpu_actions = action.cpu()
         cpu_actions = cpu_actions.numpy()
 
-        self.state, reward, done, info = self.env.step(cpu_actions)
-        self.state = self.state_to_torch(self.state)
+        state, reward, done, info = self.env.step(cpu_actions)
         self.rewards.append(reward)
 
         return done
 
-    def state_to_torch(self, state):
-        state = np.stack(state)
-        state = np.asarray([state, state, state, state])
-        state = torch.from_numpy(state).float()
-        state = state.permute(1, 0, 2, 3)
-        state.cuda()
-        return state
+    def states_to_torch(self, states):
+        states = np.stack(states)
+        states = torch.from_numpy(states).float()
+        states = states.permute(1, 0, 2, 3)
+        states.cuda()
+        return states
 
     def reset(self):
-        self.state = self.env.reset()
-        self.state = self.state_to_torch(self.state)
+        state = self.env.reset()
+        self.states.append(state)
+        self.states.append(state)
+        self.states.append(state)
+        self.states.append(state)
         self.rewards = []
